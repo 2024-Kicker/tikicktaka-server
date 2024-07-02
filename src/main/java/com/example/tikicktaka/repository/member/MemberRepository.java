261e0dbac2621fa2bc6264e39e6314968baee3e6
jjookki33 <jjookki33@gmail.com>
2024-07-02 23:34:09 +0900

    feature: 회원가입 api 구현

--- src/main/java/com/example/tikicktaka/repository/member/MemberRepository.java
+++ src/main/java/com/example/tikicktaka/repository/member/MemberRepository.java
@@ -3,5 +3,13 @@ package com.example.tikicktaka.repository.member;
 import com.example.tikicktaka.domain.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByNickname(String nickname);
+
+    Optional<Member> findByLoginId(String loginId);
+
+    Optional<Member> findByPhone(String phone);
 }
--- src/main/java/com/example/tikicktaka/web/dto/member/MemberResponseDTO.java
+++ src/main/java/com/example/tikicktaka/web/dto/member/MemberResponseDTO.java
@@ -1,4 +1,23 @@
 package com.example.tikicktaka.web.dto.member;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
 public class MemberResponseDTO {
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class JoinResultDTO{
+
+        Long memberId;
+        String nickname;
+        String loginId;
+        LocalDateTime createdAt;
+    }
 }
