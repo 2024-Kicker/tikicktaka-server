d750441f45750594b1eeef58280d123cb10c0e9c
jjookki33 <jjookki33@gmail.com>
2024-08-25 02:55:43 +0900

    feat: 랜선투어 상품 리뷰 전체조회 api

--- src/main/java/com/example/tikicktaka/service/lanTourService/LanTourQueryService.java
+++ src/main/java/com/example/tikicktaka/service/lanTourService/LanTourQueryService.java
@@ -1,10 +1,13 @@
 package com.example.tikicktaka.service.lanTourService;
 
 import com.example.tikicktaka.domain.lanTour.LanTour;
+import com.example.tikicktaka.domain.lanTour.Review;
 import org.springframework.data.domain.Page;
 
 public interface LanTourQueryService {
     LanTour getLanTourContent(Long lanTourId);
 
     Page<LanTour> getLanTourList(String region, String orderType, Integer page);
+
+    Page<Review> getReviewList(Long lanTourId, Integer page);
 }
