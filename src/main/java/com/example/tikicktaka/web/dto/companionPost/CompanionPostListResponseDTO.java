8be325e99b35c15e4c1bf190229f01287f9826e6
hyojeongchoi <63584457+hyojeongchoi@users.noreply.github.com>
2025-09-21 16:47:17 +0900

    feat: 동행찾기 게시글 목록 api 수정- 작성날짜, 게시글 상태, 작성 여부 반환

--- src/main/java/com/example/tikicktaka/web/dto/companionPost/CompanionPostListResponseDTO.java
+++ src/main/java/com/example/tikicktaka/web/dto/companionPost/CompanionPostListResponseDTO.java
@@ -3,6 +3,8 @@ package com.example.tikicktaka.web.dto.companionPost;
 import com.example.tikicktaka.domain.companionPost.CompanionPost;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class CompanionPostListResponseDTO {
     private Long id;
@@ -11,15 +13,25 @@ public class CompanionPostListResponseDTO {
     private String thumbnailUrl;
     private String authorName;
     private boolean isScraped; // 스크랩 여부
+    private LocalDateTime createdAt;           // 작성 날짜
+    private boolean isMine;                    // 로그인 사용자가 작성한 글인지
+    private CompanionPost.PostStatus status;   // FINDING / FOUND
 
 
-    public CompanionPostListResponseDTO(CompanionPost post, boolean isScraped) {
+    public CompanionPostListResponseDTO(CompanionPost post, boolean isScraped, Long memberID) {
         this.id = post.getId();
         this.title = post.getTitle();
         this.content = post.getContent();
         this.thumbnailUrl = post.getThumbnailUrl();  // 대표 이미지 (썸네일) 설정
         this.authorName = post.getAuthor().getName();
         this.isScraped = isScraped;
+        this.createdAt = post.getCreatedAt();
+        this.status = post.getStatus();
+        this.isMine = (memberID != null
+                && post.getAuthor() != null
+                && post.getAuthor().getId() != null
+                && post.getAuthor().getId().equals(memberID));
+
 
     }
 }
