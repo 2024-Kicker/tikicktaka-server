5fdf4ff77670f6f9e8e5fd9a599686527a6ca322
hyojeongchoi <63584457+hyojeongchoi@users.noreply.github.com>
2025-09-28 02:31:31 +0900

    feat: 이야기방 입장 api - 중복 입장 방지

--- src/main/java/com/example/tikicktaka/repository/storyRoom/StoryRoomParticipantRepository.java
+++ src/main/java/com/example/tikicktaka/repository/storyRoom/StoryRoomParticipantRepository.java
@@ -3,9 +3,13 @@ package com.example.tikicktaka.repository.storyRoom;
 import com.example.tikicktaka.domain.storyRoom.StoryRoomParticipant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface StoryRoomParticipantRepository extends JpaRepository<StoryRoomParticipant, Long> {
     int countByStoryRoomId(Long id);
     void deleteByStoryRoomIdAndMemberId(Long storyRoomId, Long memberId);
     boolean existsByStoryRoomIdAndMemberId(Long id, Long memberId);
+    Optional<StoryRoomParticipant> findByStoryRoom_IdAndMember_Id(Long storyRoomId, Long memberId);
+
 }
 
